5efb8453d25e1ce150fc4bb51e7a2cba7575fcbd
cN3rd <subs.in.tokyo@gmail.com>
2025-08-11 20:12:08 +0300

    Adjust asmdefs

---
+++ Assets/Scripts/Client/PlayerInput.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using Fusion;
+using Fusion.Sockets;
+using Game.Client.Input;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace Game.Client
+{
+    public enum PlayerInputButtons
+    {
+        SprintButton,
+        PlaceBombButton
+    }
+
+    [RequireComponent(typeof(NetworkTransform))]
+    public class PlayerInput : NetworkBehaviour, INetworkRunnerCallbacks
+    {
+        private InputSystemActions _inputSystemActions;
+
+        private Vector2 _move;
+
+        private void Awake() => _inputSystemActions = new InputSystemActions();
+
+        private void OnEnable()
+        {
+            if (!HasInputAuthority || !HasStateAuthority) return;
+
+            _inputSystemActions.Player.Move.performed += OnMovePerformed;
+            _inputSystemActions.Player.Move.canceled += OnMoveCancelled;
+
+            _inputSystemActions.Enable();
+
+            Runner.AddCallbacks(this);
+        }
+
+        private void OnDisable()
+        {
+            if (!HasInputAuthority || !HasStateAuthority) return;
+
+            _inputSystemActions.Disable();
+
+            _inputSystemActions.Player.Move.performed += OnMovePerformed;
+            _inputSystemActions.Player.Move.canceled += OnMoveCancelled;
+
+            Runner.RemoveCallbacks(this);
+        }
+
+        private void OnMovePerformed(InputAction.CallbackContext ctx) => _move = ctx.ReadValue<Vector2>();
+        private void OnMoveCancelled(InputAction.CallbackContext ctx) => _move = Vector2.zero;
+
+        public void OnInput(NetworkRunner runner, NetworkInput input)
+        {
+            var inputState = new PlayerInputState { move = _move };
+            inputState.buttons.Set(PlayerInputButtons.SprintButton, _inputSystemActions.Player.Sprint.IsPressed());
+            inputState.buttons.Set(PlayerInputButtons.PlaceBombButton, _inputSystemActions.Player.PlaceBomb.IsPressed());
+            input.Set(inputState);
+        }
+
+        #region Empty INetworkRunnerCallbacks functions
+        // other network runner callbacks, intentionally empty
+        public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) {}
+
+        public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player) {}
+
+        public void OnPlayerJoined(NetworkRunner runner, PlayerRef player) {}
+
+        public void OnPlayerLeft(NetworkRunner runner, PlayerRef player) {}
+
+        public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason) {}
+
+        public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason) {}
+
+        public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token) {}
+
+        public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason) {}
+
+        public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message) {}
+
+        public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data) {}
+
+        public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress) {}
+
+        public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input) {}
+        public void OnConnectedToServer(NetworkRunner runner) {}
+
+        public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList) {}
+
+        public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data) {}
+
+        public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken) {}
+
+        public void OnSceneLoadDone(NetworkRunner runner) {}
+
+        public void OnSceneLoadStart(NetworkRunner runner) {}
+        #endregion
+    }
+
+    public class PlayerInputSender : MonoBehaviour, INetworkInput {}
+
+    public struct PlayerInputState : INetworkInput
+    {
+        public Vector2 move;
+        public NetworkButtons buttons;
+    }
+}
---
+++ Assets/Scripts/Client/PlayerScript.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Fusion;
+using UnityEngine;
+
+namespace Game.Client
+{
+    [RequireComponent(typeof(NetworkMecanimAnimator))]
+    [RequireComponent(typeof(PlayerInput))]
+    public class PlayerScript : NetworkBehaviour
+    {
+        [SerializeField] private PlayerInput playerInput;
+        [SerializeField] private Animator animator;
+        [SerializeField] private float speed = 5f;
+
+        private bool _isSprinting;
+        private NetworkButtons _prevButtons;
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (!animator)
+            {
+                animator = GetComponentInChildren<Animator>();
+            }
+
+            if (!playerInput)
+            {
+                playerInput = GetComponent<PlayerInput>();
+            }
+        }
+#endif
+
+        public override void FixedUpdateNetwork()
+        {
+            if (!GetInput(out PlayerInputState input))
+            {
+                return;
+            }
+
+            transform.Translate(input.move * (speed * Runner.DeltaTime));
+
+            // Only done on host:
+            if (Object.HasStateAuthority)
+            {
+                if (input.buttons.WasPressed(_prevButtons, PlayerInputButtons.PlaceBombButton))
+                {
+                    Debug.Log("Place bomb");
+                }
+
+                if (input.buttons.WasPressed(_prevButtons, PlayerInputButtons.SprintButton))
+                {
+                    Debug.Log("sprint began!");
+                }
+                else if (input.buttons.WasReleased(_prevButtons, PlayerInputButtons.SprintButton))
+                {
+                    Debug.Log("sprint ended!");
+                }
+            }
+
+            _prevButtons = input.buttons;
+        }
+    }
+}
